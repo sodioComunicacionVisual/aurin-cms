16ed447db9faef8e0db1a9692aaf1fac8c29f01b
Karen Rebeca Ortiz ✨ <Karen.ortizg@yahoo.com>
2025-10-20 18:06:53 -0600

    Revert "Add Vercel Blob Storage integration for media uploads"
    This reverts commit 03d83ca19a54c33101a174967de06f4d20bdb003.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,4 @@
 // storage-adapter-import-placeholder
-import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
@@ -56,12 +55,6 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    vercelBlobStorage({
-      enabled: true,
-      collections: {
-        media: true,
-      },
-      token: process.env.BLOB_READ_WRITE_TOKEN || '',
-    }),
+    // storage-adapter-placeholder
   ],
 })
